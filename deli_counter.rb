97c57d007c45f5966f3027fb34f9578a9306f989
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-14 05:32:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,10 @@ def line(katz_deli)
     if katz_deli.empty?
       puts "The line is currently empty."
     else
-      katz_deli.each do |customers|
-      puts "[] + #{brother}!"
+      position = 1
+      katz_deli.each do |customer|
+      puts "#{position}. #{customer}"
+      position +=1
       end
     end
 end
