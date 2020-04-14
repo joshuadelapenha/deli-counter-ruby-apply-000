26b00aa203340abc33d02e751f2e9b95622f6281
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-14 06:11:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,3 +20,10 @@ def take_a_number(katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+    
+  end 
+end
