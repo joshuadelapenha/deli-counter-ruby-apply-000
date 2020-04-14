e25c62d5eb202e6077670a15e35334388e1c4361
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-14 05:43:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,11 @@ def line(katz_deli)
       puts "The line is currently empty."
     else
       position = 1
+      the_line = "The line is currently: "
       katz_deli.each do |customer|
-      puts "#{position}. #{customer}"
+      the_line << "#{position}. #{customer} "
       position +=1
       end
     end
+    puts the_line
 end
